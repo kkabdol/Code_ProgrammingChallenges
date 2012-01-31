eb60f45dcaae22c3f91b68e045c03907c82110aa
Seonghyeon Choe <kkabdol@gmail.com>
2012-01-31 19:50:31 +0900

    10189 added

---
+++ Problem10189/Problem10189/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Problem10189
+//
+//  Created by Seonghyeon Choe on 1/31/12.
+//  Copyright (c) 2012 kkabdol@gmail.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
