337816884eb32314a45e7a3d413564bdb3066862
Seonghyeon Choe <kkabdol@gmail.com>
2012-05-10 08:46:45 +0900

    Project setting changed

--- Problem10189/Problem10189/main.cpp
+++ Problem10189/Problem10189/main.cpp
@@ -2,17 +2,36 @@
 //  main.cpp
 //  Problem10189
 //
+//  Minesweeper
+//
 //  Created by Seonghyeon Choe on 1/31/12.
 //  Copyright (c) 2012 kkabdol@gmail.com. All rights reserved.
 //
 
 #include <iostream>
+using namespace std;
+
+void makeMineMap(int n, int m);
 
 int main (int argc, const char * argv[])
 {
+    int n, m;
+    int count = 0;
+    
+    while(cout >> n >> m) {
+        if (n == 0 && m == 0) {
+            break;
+        }
+        
+        cout << "Field #" << ++count << ":" << endl;
+        makeMineMap(n, m);
+        cout << endl;
+    }
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
 
+void makeMineMap(int n, int m)
+{
+    
+}
